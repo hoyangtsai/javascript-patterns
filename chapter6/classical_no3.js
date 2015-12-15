a932c7787a6774d401ae9c0cecb684437cc16c10
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-12-15 16:37:01 +0800

    Classical #3 update

--- chapter6/classical_no3.js
+++ chapter6/classical_no3.js
@@ -1,3 +1,5 @@
+// 借用並設定原型
+
 // 父建構式
 function Parent(name) {
   this.name = name || 'Adam';
@@ -8,18 +10,15 @@ Parent.prototype.say = function() {
   return this.name;
 };
 
-function inherit(C, P) {
-  C.prototype = new P();
-}
-
 function Child(name) {
   Parent.apply(this, arguments);
 }
 Child.prototype = new Parent();
 
 var kid = new Child("Patrick");
-// kid.name = "Patrick"; // Patrick
+kid.name = "Patrick"; // Patrick
 console.log(kid.say()); // Patrick
-
 delete kid.name;
 console.log(kid.say()); // Adam
+
+// inherit parent constructor and functions without calling "inherit" function every time
