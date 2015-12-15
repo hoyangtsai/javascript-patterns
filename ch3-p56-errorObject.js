47cc963ed6f1e60dafb9cf6cf35fc7fd32ba9888
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-12-15 17:34:20 +0800

    Update error object script

--- ch3-p56-errorObject.js
+++ ch3-p56-errorObject.js
@@ -1,16 +1,14 @@
-//try {
-//  throw {
-//    name: "MyErrorType",
-//    message: "opps",
-//    extra: "This was rather embarrassing"
-////    remedy: genericErrorHandler
-//  };
-//} catch (e) {
-//  console.log(e.extra);
-//  
-////  e.remedy();
-//}
-
+try {
+ throw {
+   name: "MyErrorType",
+   message: "opps",
+   extra: "This was rather embarrassing"
+//    remedy: genericErrorHandler
+ };
+} catch (e) {
+ console.log(e.extra);
+//  e.remedy();
+}
 
 try {
   throw SyntaxError("Input is not a number");
